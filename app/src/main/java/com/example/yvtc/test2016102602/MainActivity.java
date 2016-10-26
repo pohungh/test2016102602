b2c769dcf9da145f31fc7daa8a8947f56c8db52f
pohung <kof96302@yahoo.com.tw>
2016-10-26 20:30:01 +0800

    Add spinner

--- app/src/main/java/com/example/yvtc/test2016102602/MainActivity.java
+++ app/src/main/java/com/example/yvtc/test2016102602/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    String fruits[] = {"芭樂","芒果","荔枝","甘蔗","鳳梨"};
+    String fruits[] = {"芭樂", "芒果", "荔枝", "甘蔗", "鳳梨"};
     Spinner spinner;
     ArrayAdapter<String> adapter;
     @Override
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner);
 
         adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_spinner_item,
-                fruits);
+                        android.R.layout.simple_spinner_item,
+                        fruits);
         spinner.setAdapter(adapter);
+
     }
 }
